Fixed qsd_comparison and total variation distance calculation for new eig-based implementation

Circuit_Examples/simple_switch.py
@@ -1,38 +1,53 @@
 import matplotlib.pyplot as plt
 from mobspy import *
 import numpy as np
+import os
+import sys
 
+from sympy.core.assumptions_generated import beta_rules
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from functional_compiler import q_compile, q_matrix_generator, bound_estimator, Jump_chain_qsd_bound
 import pickle as pkl
 import utils_rare_event as ure
 
-f_1, f_2, f_3 = 2, 0, 1/3
-initial_count, promoter_number = 30, 30
-P1, P2, Mortal = BaseSpecies()
-S1, S2 = New(Mortal)
 
-Mortal >> Zero [0.1]
+def generate_mobspy_switch_model(initial_count, promoter_number= 30,
+                                 hill_coef = 2, alpha =  0.01, beta = 0.0002,
+                                 f_1 = 2, f_2 = 1/2, f_3 = 1/10, return_species = False):
+    P1, P2, Mortal = BaseSpecies()
+    S1, S2 = New(Mortal)
 
-S2 + P1 >> S2 + P1 + S1[lambda s, p: 0.1*p*1/(1 + (s/10)**2)]
-S1 + P2 >> S1 + P2 + S2[lambda s, p: 0.1*p*1/(1 + (s/10)**2)]
 
-S1(initial_count), S2(0), P1(promoter_number), P2(promoter_number)
-Sim = Simulation(S1 | S2 | P1 | P2)
+    Mortal >> Zero [beta]
 
-def run_model():
-    Sim.duration = 10
+    S2 + P1 >> S2 + P1 + S1[lambda s, p: alpha*p*1/(1 + (s/10)**hill_coef)]
+    S1 + P2 >> S1 + P2 + S2[lambda s, p: alpha*p*1/(1 + (s/10)**hill_coef)]
+
+    S1(initial_count), S2(0), P1(promoter_number), P2(promoter_number)
+    Sim = Simulation(S1 | S2 | P1 | P2)
+
+    interval = {S1: [int(initial_count * f_2), int(initial_count* f_1)],
+                S2: [0, int(initial_count * f_3)],
+                P1: [promoter_number, promoter_number],
+                P2: [promoter_number, promoter_number]}
+    exit_direction = {S1: 'below'}
+
+    if return_species:
+        return Sim, interval, exit_direction, [S1, S2]
+    else:
+        return Sim, interval, exit_direction
+
+
+def run_model(inital_count = 30, promoter_number =30, duration = 10):
+    Sim, _, _ = generate_mobspy_switch_model(initial_count=inital_count, promoter_number=promoter_number)
+    Sim.duration = duration
     Sim.run()
 # run_model()
 
 def bound_calculation(initial_state):
-    S1(initial_state), S2(0), P1(promoter_number), P2(promoter_number)
-
-    interval = {S1: [int(initial_count * f_2), int(initial_count * f_1)],
-                S2: [0, int(initial_count * f_3)],
-                P1: [promoter_number, promoter_number],
-                P2: [promoter_number, promoter_number]}
-    exit_direction_s1 = {S1: 'below'}
+    Sim, interval, exit_direction_s1 = generate_mobspy_switch_model(initial_count=initial_state)
     J = Jump_chain_qsd_bound(Sim, interval, exit_direction=exit_direction_s1)
     J.calculate_bound()
 # bound_calculation(30)
@@ -41,15 +56,10 @@ def bound_calculation(initial_state):
 def A_data_generation():
     results = []
     for i in [50, 30, 10, 0]:
-        S1(i), S2(0), P1(promoter_number), P2(promoter_number)
+        Sim, interval, exit_direction_s1 = generate_mobspy_switch_model(initial_count=i)
 
-        interval = {S1: [int(initial_count * f_3), int(initial_count * f_1)],
-                    S2: [0, int(initial_count * f_3)],
-                    P1: [promoter_number, promoter_number],
-                    P2: [promoter_number, promoter_number]}
-        exit_direction_s1 = {S1: 'below'}
         J = Jump_chain_qsd_bound(Sim, interval, exit_direction=exit_direction_s1)
-        A, decay = J.calculate_bound(probability_epsilon_bound=1e-7, stopping_probability=1e-7)
+        A, decay = J.calculate_bound(probability_epsilon_bound=1e-10, stopping_probability=1e-10)
         results.append((A, decay))
 
     with open('figures/A_decay_res.pkl', 'wb') as f:
@@ -57,20 +67,19 @@ def A_data_generation():
 
     with open('figures/A_decay_res.pkl', 'rb') as f:
         res = pkl.load(f)
-    print(res)
 # A_data_generation()
 
 
 def table_gen_from_A_data():
-    latex_code = '\\begin{table}[h!]\n' + '\centering\n' + '\label{table:A_value_switch}\n' + \
-                 '\\begin{tabular}{|l|l|}\n' \
-                 + '\hline\n' + 'Initial Value S1 & A \\\\\n'
+    latex_code = r'\begin{table}[h!]' + '\n' + r'\centering' + '\n' + r'\label{table:A_value_switch}' + '\n' + \
+                 r'\begin{tabular}{|l|l|}' + '\n' \
+                 + r'\hline' + '\n' + r'Initial Value S1 & A \\' + '\n'
 
     with open('figures/A_decay_res.pkl', 'rb') as f:
         res = pkl.load(f)
     for i, A in zip([50, 30, 10, 0], res):
-        latex_code = latex_code + f'\hline$({i}, 0)$ & ${ure.round_to_significant(A[0], 2)}$ \\\\\n'
-    latex_code = latex_code + '\hline\n' + '\\end{tabular}\n' + '\\end{table}\n'
+        latex_code = latex_code + f'\\hline$({i}, 0)$ & ${ure.round_to_significant(A[0], 2)}$ \\\\\n'
+    latex_code = latex_code + r'\hline' + '\n' + r'\end{tabular}' + '\n' + r'\end{table}' + '\n'
     print(latex_code)
     print()
     print('Decay: ' + str(res[0][1]))
@@ -78,66 +87,79 @@ def table_gen_from_A_data():
 
 
 def qsd_prediction():
-    interval = {S1: [0, int(initial_count*f_1)],
-                S2: [0, int(initial_count*f_3)],
-                P1: [promoter_number, promoter_number],
-                P2: [promoter_number, promoter_number]}
-    exit_direction_s1 = {S1: 'below'}
+    initial_count = 30
+    promoter_number = 30
+    f_1 = 2
+    f_2 = 0
+    f_3 = 1/3
 
-    S1(initial_count), S2(0), P1(promoter_number), P2(promoter_number)
-    Sim1 = Simulation(S1 | S2 | P1 | P2)
+    # HIGH S1 QSD (V1 region)
+    Sim1, interval_v1, exit_direction_s1, [S1, S2] = generate_mobspy_switch_model(
+        initial_count=initial_count,
+        promoter_number=promoter_number,
+        alpha=0.001, beta=0.001,
+        hill_coef=4,
+        f_1=f_1, f_2=f_2, f_3=f_3,
+        return_species = True
+    )
 
-    J = Jump_chain_qsd_bound(Sim1, interval, exit_direction=exit_direction_s1)
-    partial = J.partial_qsd(S1)
-    plt.plot([i for i in range(0, int(f_1*initial_count) + 1)], partial, label='High S1 QSD')
+    J1 = Jump_chain_qsd_bound(Sim1, interval_v1, exit_direction=exit_direction_s1)
 
-    S1(0), S2(initial_count), P1(promoter_number), P2(promoter_number)
-    Sim2 = Simulation(S1 | S2 | P1 | P2)
+    partial_high_s1 = J1.partial_qsd(S1)
+    partial_s2 = J1.partial_qsd(S2)
+    plt.plot(range(len(partial_high_s1)), partial_high_s1, label='S1 QSD')
+    plt.plot(range(len(partial_s2)), partial_s2, label='S2 QSD')
 
-    interval = {S1: [0, int(initial_count*f_3)],
-                S2: [0, int(f_1*initial_count)],
-                P1: [promoter_number, promoter_number],
-                P2: [promoter_number, promoter_number]}
-    exit_direction_s2 = {S2: 'below'}
 
-    J = Jump_chain_qsd_bound(Sim2, interval, exit_direction=exit_direction_s2)
-    partial = J.partial_qsd(S1)
-    plt.plot([i for i in range(0,  int(f_3*initial_count) + 1)], partial, label='Low S1 QSD')
-
-    plt.title('QSDs of the bistable switch BCRN')
-    plt.xlabel('S1 state probability')
-    plt.ylabel('S1 (counts)')
+    plt.title('QSDs of the bistable switch BCRN', fontsize=16)
+    plt.xlabel('Counts', fontsize=14)
+    plt.ylabel('State probability', fontsize=14)
     plt.legend()
     plt.savefig("figures/QSD_Switch.png")
     plt.show()
-# qsd_prediction()
+
 
 
 def qsd_comparison():
-    total_interval = {S1: [0, int(initial_count*f_1)],
-                      S2: [0, int(initial_count*f_1)],
-                      P1: [promoter_number, promoter_number],
-                      P2: [promoter_number, promoter_number]}
-    exit_direction = {S1: 'below', S2: 'below'}
+    initial_count = 30
+    promoter_number = 30
 
-    Jo = Jump_chain_qsd_bound(Sim, total_interval, exit_direction=exit_direction)
+    # Create the full system (both regions combined)
+    Sim_full, total_interval, exit_direction = generate_mobspy_switch_model(
+        initial_count=initial_count,
+        promoter_number=promoter_number,
+        alpha=0.001, beta=0.001,
+        hill_coef=4,
+        f_1=2, f_2=0, f_3=2 # Full space: S1=[0, 60], S2=[0, 60]
+    )
+
+    Jo = Jump_chain_qsd_bound(Sim_full, total_interval, exit_direction=exit_direction)
     Q = Jo.q_matrix[:-1, :-1]
-    d = np.zeros(len(Q))
+
+    # Initialize distribution at (30, 0) state
+
+    d = np.zeros(max(Q.shape))
     for state in Jo.state_to_index:
-        if state[2] == 30 and state[3] == 0:
+        if state[2] == 30 and state[3] == 0:  # (S1=30, S2=0)
             d[Jo.state_to_index[state]] = 1
             break
+
+    # Evolve the distribution
     d = ure.calculate_distribution(d, 100000, Q)
 
-    interval = {S1: [0, int(initial_count*f_1)],
-                S2: [0, int(initial_count*f_3)],
-                P1: [promoter_number, promoter_number],
-                P2: [promoter_number, promoter_number]}
-    exit_direction = {S1: 'below'}
+    # Create the cut process for V1 region
+    Sim_cut, interval, exit_direction = generate_mobspy_switch_model(
+        initial_count=initial_count,
+        promoter_number=promoter_number,
+        alpha=0.001, beta=0.001,
+        hill_coef=4,
+        f_1=2, f_2=0, f_3=1/3  # V1: S1=[0, 60], S2=[0, 10]
+    )
 
-    J1 = Jump_chain_qsd_bound(Sim, interval, exit_direction=exit_direction)
+    J1 = Jump_chain_qsd_bound(Sim_cut, interval, exit_direction=exit_direction)
     qsd = J1.calculate_qsd()
 
+    # Compare the distributions
     distance = 0
     x = []
     y = []
@@ -151,12 +173,18 @@ def qsd_comparison():
             distance += d_step
         x.append(io)
         y.append(d_step)
+
     plt.plot(x, y)
+    plt.xlabel('State Index')
+    plt.ylabel('Probability Difference')
+    plt.title('QSD vs Time Evolution Difference')
     plt.show()
-    print(distance)
-qsd_comparison()
+    print(f"Total variation distance: {distance}")
+
 
 def Gillespiere_Comparison():
+
+    Sim, _, _ = generate_mobspy_switch_model()
 
     t = 1/4.3820718429521486e-07
     Sim.duration = t
@@ -170,23 +198,180 @@ def Gillespiere_Comparison():
 # Gillespiere_Comparison()
 
 
-def Compare_Probabilities():
+#def Compare_Probabilities():
 
-    with open('figures/switch_sim_data.pkl', 'rb') as f:
-        res = pkl.load(f)
 
-    positive_run = 0
-    for r in res[S2]:
-        for v in r:
-            if v > initial_count*f_3:
-                positive_run += 1
-                break
+    #with open('figures/switch_sim_data.pkl', 'rb') as f:
+    #    res = pkl.load(f)
 
-    print(positive_run/len(res[S2]))
-    print(np.exp(1/4.3820718429521486e-07*-4.3820718429521486e-07))
+    #positive_run = 0
+    #for r in res[S2]:
+    #    for v in r:
+    #        if v > initial_count*f_3:
+    #            positive_run += 1
+    #            break
+
+    # print(positive_run/len(res[S2]))
+    # print(np.exp(1/4.3820718429521486e-07*-4.3820718429521486e-07))
 # Compare_Probabilities()
 
 
+def population_sweep_all_hills():
+    """Test all Hill coefficients in one go"""
+
+    P_values = [2, 6, 10, 20, 40]  # Promoter levels to control proteins
+    hill_coefficients = [2, 3, 4]
+    test_times = [6 * 3600, 12 * 3600, 24 * 3600]  # 6h, 12h, 24h in seconds
+
+    alpha = 0.05
+    beta = 0.01
+
+    all_results = []
+    os.makedirs('figures', exist_ok=True)
+
+    for n_h in hill_coefficients:
+        for P in P_values:
+            initial_S1 = int(alpha / beta * P)
+            print(f"Testing P = {P}, n = {n_h}...")
+
+            Sim, interval, exit_direction = generate_mobspy_switch_model(
+                initial_count=initial_S1,
+                promoter_number=P,
+                alpha=alpha,
+                beta=beta,
+                hill_coef=n_h,
+                f_1=2, f_2=1 / 3, f_3=1/5
+            )
+
+            J = Jump_chain_qsd_bound(Sim, interval, exit_direction=exit_direction)
+            A, decay = J.calculate_bound(probability_epsilon_bound=1e-7,
+                                         stopping_probability=1e-7)
+
+            if decay > 0:
+                # Calculate transition probabilities for different times
+                probs = {}
+                for t in test_times:
+                    prob_bound = np.exp(-decay * t) - A
+                    prob_bound = max(0, prob_bound)
+                    transition_prob = 1 - prob_bound
+                    probs[f'{int(t / 3600)}h'] = transition_prob
+
+                mean_time = 1 / decay  # This is 1/λ
+            else:
+                probs = {f'{int(t / 3600)}h': 0 for t in test_times}
+                mean_time = float('inf')
+
+            all_results.append({
+                'P': P,
+                'n_h': n_h,
+                'decay': decay,
+                'A': A,
+                'P_transition_6h': probs['6h'],
+                'P_transition_12h': probs['12h'],
+                'P_transition_24h': probs['24h'],
+                'mean_time_1_over_lambda': mean_time,
+                'mean_time_readable': format_time(mean_time)
+            })
+
+
+    # Generate table with multiple time points
+    latex_code = '''\\begin{table}[h!]
+\\centering
+\\begin{tabular}{|l|l|l|l|l|l|l|l|}
+\\hline
+$P$ & $n$ & Decay Parameter $(\\lambda)$ & $A$ & $\\mathbb{P}(\\text{transition} \\leq 6\\text{h})$ & $\\mathbb{P}(\\text{transition} \\leq 12\\text{h})$ & $\\mathbb{P}(\\text{transition} \\leq 24\\text{h})$ & $1/\\lambda$ \\\\
+\\hline
+'''
+
+    for r in all_results:
+        if isinstance(r['decay'], (float, np.float64)):
+            latex_code += f"${r['P']}$ & ${r['n_h']}$ & ${ure.round_to_significant(r['decay'], 2)}$ & "
+            latex_code += f"${ure.round_to_significant(r['A'], 2)}$ & "
+            latex_code += f"${ure.round_to_significant(r['P_transition_6h'], 2)}$ & "
+            latex_code += f"${ure.round_to_significant(r['P_transition_12h'], 2)}$ & "
+            latex_code += f"${ure.round_to_significant(r['P_transition_24h'], 2)}$ & "
+            latex_code += f"{r['mean_time_readable']} \\\\\n"
+        else:
+            latex_code += f"${r['P']}$ & ${r['n_h']}$ & Error & Error & Error & Error & Error & Error \\\\\n"
+
+    latex_code += '''\\hline
+\\end{tabular}
+\\caption{Toggle Switch Robustness Analysis. Transition probabilities are calculated as $1 - \\max(0, e^{-\\lambda t} - A)$ for different time intervals. The last column shows $1/\\lambda$ which represents the characteristic time scale of the QSD.}
+\\label{table:hill_comparison}
+\\end{table}
+'''
+
+    with open('figures/hill_coefficient_sweep.pkl', 'wb') as f:
+        pkl.dump(all_results, f)
+
+    print(latex_code)
+
+
+def format_time(seconds):
+    """Convert seconds to readable format"""
+    if seconds == float('inf'):
+        return "∞"
+    elif seconds < 60:
+        return f"{seconds:.1f} sec"
+    elif seconds < 3600:
+        return f"{seconds / 60:.1f} min"
+    elif seconds < 86400:
+        return f"{seconds / 3600:.1f} hours"
+    elif seconds < 31536000:
+        return f"{seconds / 86400:.1f} days"
+    else:
+        return f"{seconds / 31536000:.1f} years"
+
+
+def generate_hill_comparison_table(reculate=True):
+    """Generate LaTeX table comparing different Hill coefficients"""
+    if reculate:
+        population_sweep_all_hills()
+
+    with open('figures/hill_coefficient_sweep.pkl', 'rb') as f:
+        all_results = pkl.load(f)
+
+    # Generate one combined table with consistent units (hours)
+    latex_code = '''\\begin{table}[h!]
+\\centering
+\\begin{tabular}{|l|l|l|l|l|l|}
+\\hline
+P & n & Decay Parameter & A & P(transition in 24h) & Mean Time (hours) \\\\
+\\hline
+'''
+
+    for r in all_results:
+        if isinstance(r['decay'], (float, np.float64)):
+            # Convert mean time to hours consistently
+            mean_time_hours = r['mean_time_sec'] / 3600 if r['mean_time_sec'] != float('inf') else float('inf')
+
+            if mean_time_hours == float('inf'):
+                mean_time_str = '$\\infty$'
+            else:
+                mean_time_str = f'${ure.round_to_significant(mean_time_hours, 2)}$'
+
+            latex_code += f"${r['P']}$ & ${r['n_h']}$ & ${ure.round_to_significant(r['decay'], 2)}$ & "
+            latex_code += f"${ure.round_to_significant(r['A'], 2)}$ & "
+            latex_code += f"${ure.round_to_significant(r['P_transition_24h'], 2)}$ & "
+            latex_code += f"{mean_time_str} \\\\\n"
+        else:
+            latex_code += f"${r['P']}$ & ${r['n_h']}$ & Error & Error & Error & Error \\\\\n"
+
+    latex_code += '''\\hline
+\\end{tabular}
+\\caption{Toggle Switch Robustness vs Hill Coefficient and Promoter Copy Number}
+\\label{table:hill_comparison}
+\\end{table}
+'''
+
+    print(latex_code)
+
+
+# Run the analysis
 if __name__ == '__main__':
-    bound_calculation(30)
+    population_sweep_all_hills()
+    # qsd_prediction()
+    # qsd_comparison()
+
+
 

functional_compiler.py
@@ -188,20 +188,27 @@ def q_matrix_generator(set_species_model, reactions_for_q, interval, exit_direct
     for i, comb in enumerate(itertools.product(*all_value_combinations)):
         state_index_to_spe[comb], spe_to_state_index[i] = i, comb
 
-    row_gen = lambda i, state: q_row_generation(state, reactions_for_q, state_index_to_spe, species_order_index,
-                                                rev_exit_direction, assignments)
+    n_states = len(state_index_to_spe)
+    q_matrix = lil_matrix((n_states + 1, n_states + 1))  # +1 for absorbing state
 
-    q_matrix = Parallel(n_jobs=1, prefer="threads")(delayed(row_gen)(state_index_to_spe[state], state)
-                                                    for state in state_index_to_spe.keys())
+    # Generate and insert rows one at a time
+    for state, i in state_index_to_spe.items():
+        row = q_row_generation(state, reactions_for_q, state_index_to_spe,
+                               species_order_index, rev_exit_direction, assignments)
 
-    q_matrix = np.array(q_matrix)
-    absorbing_row = np.zeros((1, q_matrix.shape[1]))  # Match the number of columns
-    q_matrix = np.vstack([q_matrix, absorbing_row])
+        # Insert non-zero elements directly into sparse matrix
+        row_sum = 0
+        for j, val in row.items():
+            if j == -1:  # Absorbing state
+                q_matrix[i, n_states] = val
+            else:
+                q_matrix[i, j] = val
+            row_sum += val
 
-    for i, row in enumerate(q_matrix):
-        q_matrix[i, i] = - sum(row)
+        # Set diagonal
+        q_matrix[i, i] = -row_sum
 
-    q_matrix = csr_matrix(q_matrix)
+    q_matrix = q_matrix.tocsr()
 
     return q_matrix, state_index_to_spe, species_order_index, assignments
 
@@ -209,7 +216,8 @@ def q_matrix_generator(set_species_model, reactions_for_q, interval, exit_direct
 def q_row_generation(state, reactions_for_q, state_index_to_spe, species_order_index, rev_exit_direction,
                      assignments):
 
-    q_row = np.zeros(len(state_index_to_spe) + 1)
+    q_row_dict = {}
+    q_row_dict[-1] = 0
 
     # TODO not compatible with meta-species with queries
     assignements_pre_order = []
@@ -243,11 +251,19 @@ def q_row_generation(state, reactions_for_q, state_index_to_spe, species_order_i
         new_state = tuple(new_state[0:len(state)])
 
         # If the rate is inside interval, we add it to the state transition
+        to_absorbing_state = False
         try:
             j = state_index_to_spe[new_state]
-            q_row[j] = q_row[j] + q_value
-        # If not, we add it to the final absorbing state
         except KeyError:
+            to_absorbing_state = True
+
+        if not to_absorbing_state:
+            try:
+                q_row_dict[j] = q_row_dict[j] + q_value
+            except KeyError:
+                q_row_dict[j] = q_value
+        else:
+        # If not, we add it to the final absorbing state
             flag = False
             for key in rev_exit_direction:
                 new_value = new_state[key]
@@ -258,9 +274,9 @@ def q_row_generation(state, reactions_for_q, state_index_to_spe, species_order_i
                     flag = True
                     break
             if not flag:
-                q_row[-1] = q_row[-1] + q_value
+                q_row_dict[-1] = q_row_dict[-1] + q_value
 
-    return q_row
+    return q_row_dict
 
 
 def process_initial_counts_dict(initial_counts):
@@ -364,7 +380,7 @@ def construct_jump_chain(q_matrix):
 
 def bound_estimator(initial_counts, state_index_to_spe, q_matrix, species_order_index, assignments=(),
                     time_for_stationary_calculation=1e10,
-                    max_iterations_per_diameter=1000, animation=False,
+                    max_iterations_per_diameter=5, animation=False,
                     probability_epsilon_bound=1e-5,
                     stopping_probability=1e-5,
                     time_for_decay_estimation=1,
@@ -382,21 +398,11 @@ def bound_estimator(initial_counts, state_index_to_spe, q_matrix, species_order_
     # The conditional q_matrix refers to the probability of not leaving the interval through the absorbed state
     # The absorbed state is represented by the last index in the matrix
 
-    conditional_q_matrix = deepcopy(q_matrix)
-    conditional_q_matrix = lil_matrix(conditional_q_matrix)
-    # Remove row and col leading to absorbing state
-    conditional_q_matrix = conditional_q_matrix[:-1, :-1]
-
-    # Remove last row and column (absorbing state)
-    # conditional_q_matrix = conditional_q_matrix[:-1, :-1]
-
-    for i in range(conditional_q_matrix.shape[0]):
-        conditional_q_matrix[i, i] = 0
-        row = conditional_q_matrix[i, :]
-        conditional_q_matrix[i, i] = -row.sum()
-
-    conditional_q_matrix = csr_matrix(conditional_q_matrix)
-
+    conditional_q_matrix = q_matrix[:-1, :-1].tolil()
+    conditional_q_matrix.setdiag(0)  # Set diagonal to zeros
+    row_sums = np.array(conditional_q_matrix.sum(axis=1)).flatten()  # Sum rows (now excluding diagonal)
+    conditional_q_matrix.setdiag(-row_sums)  # Set diagonal to negative row sums
+    conditional_q_matrix = conditional_q_matrix.tocsr()
 
     bol, forward_states, backward_states = check_connectivity(conditional_q_matrix)
     if bol:
@@ -429,7 +435,9 @@ def bound_estimator(initial_counts, state_index_to_spe, q_matrix, species_order_
         qsd = np.abs(qsd)
         qsd = qsd / qsd.sum()
 
+
         residual = qsd @ conditional_q_matrix
+
         if sum(residual) > 1e-13:
             raise Exception('Could not find the zero eigenvalue of the Q-matrix')
 
@@ -528,7 +536,7 @@ class Jump_chain_qsd_bound:
 
         self.bound_constant, self.decay_parameter, self.qsd = None, None, None
 
-    def calculate_bound(self, time_for_stationary_calculation=1e10, max_iterations_per_diameter=1000, animation=False,
+    def calculate_bound(self, time_for_stationary_calculation=1e10, max_iterations_per_diameter=5, animation=False,
                         probability_epsilon_bound=1e-5, stopping_probability=1e-5, time_for_decay_estimation=1):
 
         print('Starting Bound Estimation') if self.verbose else 0

utils_rare_event.py
@@ -1,5 +1,8 @@
 import scipy.linalg as sclig
 import numpy as np
+import scipy.sparse.linalg as sps_linalg # Import the sparse linear algebra module
+from scipy.sparse import csr_matrix, csc_matrix # You might explicitly check the type, but expm_multiply handles various sparse formats
+
 
 
 def round_to_significant(number, decimals=2, zero_threshold=1e-15):
@@ -46,5 +49,7 @@ def round_to_significant(number, decimals=2, zero_threshold=1e-15):
 
 
 def calculate_distribution(d, t, Q):
-    return np.matmul(d, sclig.expm(t * Q))
+    d_col_vector = d.reshape(-1, 1)
+    evolved_col_vector = sps_linalg.expm_multiply(t * Q.T, d_col_vector)
+    return evolved_col_vector.flatten()
 